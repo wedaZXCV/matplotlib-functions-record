5c5d1f2e18328a9dc029fffa0931fd4e9e9cec74
wedaSustainabilitytech <weda@sustainabilitytech.com>
2021-10-13 05:18:57 +0800

    regex add new function

--- new-function.php
+++ new-function.php
@@ -88,22 +88,54 @@
       $id = $_POST["fu-id"];
       $name = $_POST["fu-na"];
       $scod = $_POST["fu-co"];
-      $explaination = $_POST["fu-ex"];
-      $imagie = $_POST["fu-img"];
+      echo "<h4>".$scod."  </h4>";
+      // change the quotes into \" in order to be successfully able to get inserted in SQL
+      $scod = preg_replace("$\"|\'$","\\\"",$scod);
+      echo "<br>";
+      echo "<h3>".$scod."  </h3>";
+      // detect endline carrieage return
+      // $scod = preg_replace("$\n$");
+      preg_match_all("$\r$",$scod,$matches, PREG_OFFSET_CAPTURE);
+      //echo "<pre>"; print_r($matches); echo "</pre>";   // now we can detect the location of \r
+      
+      //all we need to do then is re-insert \r\n into the location as \\r\\n
+      $newLineLoc = array();
+      foreach($matches[0] as $key=>$value){
+        if($key > 0){
+          $newLineLoc[$key] = $matches[0][$key][1] + 4;
+        } else {
+          $newLineLoc[$key] = $matches[0][$key][1];
+        }
+      }
+      foreach($newLineLoc as $key=>$value){
+        $scod = substr_replace($scod, "\\r\\n", $newLineLoc[$key], 0);
+      }
+      echo "<br>new one<br>";
+      echo "<h3>".$scod."  </h3>";
 
-      //Do connection and send to MySQL server
-      include 'db-connection.php';
-      $conn = OpenCon();
-      echo "Connected Successfully";
+      //if the \r\n happens multiple of times, yields things like \r\r\r\n\n\n
+      // then we need to regex it into a single \r\n
+
       
 
-      $sql = "INSERT INTO functi VALUES($id, '$name', '$explaination', '$scod', '$imagie');";
-      if($conn->query($sql) === TRUE){
-        echo "New record created successfully";
-      } else {
-        echo "Error: ". $sql . "<br>" . $conn->error;
-      }
-      CloseCon($conn);
+      // echo "<h3> number of occurence \\n is".$occ."</h3>";
+      // echo "<h3> number of occurence \\r is".$ocd."</h3>";
+      // $explaination = $_POST["fu-ex"];
+      // $imagie = $_POST["fu-img"];
+
+      // //Do connection and send to MySQL server
+      // include 'db-connection.php';
+      // $conn = OpenCon();
+      // echo "Connected Successfully";
+      
+
+      // $sql = "INSERT INTO functi VALUES($id, '$name', '$explaination', '$scod', '$imagie');";
+      // if($conn->query($sql) === TRUE){
+      //   echo "New record created successfully";
+      // } else {
+      //   echo "Error: ". $sql . "<br>" . $conn->error;
+      // }
+      // CloseCon($conn);
     }
     
     
