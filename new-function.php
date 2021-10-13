4e6df446dbe57486e62830a3334c4aace77ab174
wedaSustainabilitytech <weda@sustainabilitytech.com>
2021-10-14 04:48:52 +0800

    new function saves CRLF

--- new-function.php
+++ new-function.php
@@ -88,13 +88,9 @@
       $id = $_POST["fu-id"];
       $name = $_POST["fu-na"];
       $scod = $_POST["fu-co"];
-      echo "<h4>".$scod."  </h4>";
       // change the quotes into \" in order to be successfully able to get inserted in SQL
       $scod = preg_replace("$\"|\'$","\\\"",$scod);
-      echo "<br>";
-      echo "<h3>".$scod."  </h3>";
       // detect endline carrieage return
-      // $scod = preg_replace("$\n$");
       preg_match_all("$\r$",$scod,$matches, PREG_OFFSET_CAPTURE);
       //echo "<pre>"; print_r($matches); echo "</pre>";   // now we can detect the location of \r
       
@@ -110,32 +106,32 @@
       foreach($newLineLoc as $key=>$value){
         $scod = substr_replace($scod, "\\r\\n", $newLineLoc[$key], 0);
       }
-      echo "<br>new one<br>";
-      echo "<h3>".$scod."  </h3>";
 
       //if the \r\n happens multiple of times, yields things like \r\r\r\n\n\n
       // then we need to regex it into a single \r\n
 
+      // but firstly first we need to remove any real \r and \n whitespace char
+
+      $scod = preg_replace("#\n|\r#", "", $scod);
+      
+      $scod = preg_replace('/(\\\r){2,}(\\\n){2,}/', "\\r\\n", $scod); // it must be three times to match \ (backslash)
+      
+      $explaination = $_POST["fu-ex"];
+      $imagie = $_POST["fu-img"];
+
+      //Do connection and send to MySQL server
+      include 'db-connection.php';
+      $conn = OpenCon();
+      echo "Connected Successfully";
       
 
-      // echo "<h3> number of occurence \\n is".$occ."</h3>";
-      // echo "<h3> number of occurence \\r is".$ocd."</h3>";
-      // $explaination = $_POST["fu-ex"];
-      // $imagie = $_POST["fu-img"];
-
-      // //Do connection and send to MySQL server
-      // include 'db-connection.php';
-      // $conn = OpenCon();
-      // echo "Connected Successfully";
-      
-
-      // $sql = "INSERT INTO functi VALUES($id, '$name', '$explaination', '$scod', '$imagie');";
-      // if($conn->query($sql) === TRUE){
-      //   echo "New record created successfully";
-      // } else {
-      //   echo "Error: ". $sql . "<br>" . $conn->error;
-      // }
-      // CloseCon($conn);
+      $sql = "INSERT INTO functi VALUES($id, '$name', '$explaination', '$scod', '$imagie');";
+      if($conn->query($sql) === TRUE){
+        echo "New record created successfully";
+      } else {
+        echo "Error: ". $sql . "<br>" . $conn->error;
+      }
+      CloseCon($conn);
     }
     
     
